b35b0af12b2093a2f88f486582c94b064c2ed5a1
Gulyás Dávid <72173665289@szily.lan>
2022-03-29 10:51:51 +0200

    Ember alkalmazás létrehozása

---
+++ Ember/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
