c5d053fc91958df81f531adb61cc7fce00fb0c8b
Gulyás Dávid <72173665289@szily.lan>
2022-03-29 11:06:50 +0200

    3 ember kiírása, 2 egyforma bizonyítása

--- Ember/Program.cs
+++ Ember/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ember
 {
@@ -6,7 +7,24 @@ namespace Ember
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Ember> list = new List<Ember>();
+            list.Add(new Ember("János", 19, "Rigó utca", 120000));
+            list.Add(new Ember("János", 19, "Rigó utca", 160000));
+            list.Add(new Ember("Petra", 32, "Rigó utca", 260000));
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            HashSet<Ember> hs = new HashSet<Ember>(list);
+            Console.WriteLine("\n halmaz után:");
+            foreach (var item in hs)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
