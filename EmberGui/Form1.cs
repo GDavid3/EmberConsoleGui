a193f695a8016c9a04c93330872dca4667870ce7
Gulyás Dávid <72173665289@szily.lan>
2022-03-29 11:40:52 +0200

    Gui Ember megtekintése, hozzáadása

--- EmberGui/Form1.cs
+++ EmberGui/Form1.cs
@@ -12,9 +12,24 @@ namespace EmberGui
 {
     public partial class Form1 : Form
     {
+        List<Ember> list;
         public Form1()
         {
             InitializeComponent();
+            list = new List<Ember>();
+            list.Add(new Ember("Péter", 18, "Róka utca", 133000));
+        }
+
+        private void Feltolt_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = list;
+        }
+
+        private void Hozzáad_Click(object sender, EventArgs e)
+        {
+            list.Add(new Ember(Nev.Text,Convert.ToInt32(Kor.Text),Cim.Text, Convert.ToInt32(Fizetes.Text)));
+            MessageBox.Show("Sikeres hozzáadás", "Hozzáad", MessageBoxButtons.OK);
         }
     }
 }
